1e5d2641a4b88cbe47f3ad26a813b0c18de976ae
Munif Tanjim <muniftanjim@gmail.com>
2021-06-06 23:41:53 +0600

    fix: set prefix for current session only
    setting global prefix affects all the sessions

--- scripts/suspend.sh
+++ scripts/suspend.sh
@@ -4,9 +4,9 @@ set -ex
 
 declare -r on_suspend_command="$1"
 
-tmux set-option -gq '@suspend_prefix' "$(tmux show-option -gqv prefix)"
+tmux set-option -q '@suspend_prefix' "$(tmux show-option -qv prefix)"
 
-tmux set-option -g prefix none \; set-option key-table suspended \; \
+tmux set-option -q prefix none \; set-option key-table suspended \; \
   if-shell -F '#{pane_in_mode}' 'send-keys -X cancel' \; \
   if-shell -F '#{pane_synchronized}' 'set synchronize-panes off'
 
