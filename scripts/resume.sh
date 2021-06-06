baa82070ec6593b3e695dda6983a86df46629b1b
Munif Tanjim <muniftanjim@gmail.com>
2021-06-06 23:41:53 +0600

    fix: set prefix for current session only
    setting global prefix affects all the sessions

--- scripts/resume.sh
+++ scripts/resume.sh
@@ -3,9 +3,13 @@
 set -ex
 
 declare -r on_resume_command="$1"
-declare -r prefix="$(tmux show-option -gqv '@suspend_prefix')"
+declare -r prefix="$(tmux show-option -qv '@suspend_prefix')"
+declare prefix_scope=""
+if [[ -z $prefix ]]; then
+  prefix_scope="u"
+fi
 
-tmux set-option -g prefix "$prefix" \; set-option -u key-table
+tmux set-option -q$prefix_scope prefix "$prefix" \; set-option -u key-table
 
 eval "$on_resume_command"
 
