97f4e91a89a0ba8842c4a25e3720553494042045
Munif Tanjim <muniftanjim@gmail.com>
2021-06-07 01:08:13 +0600

    feat: add option `@suspend_suspended_options`

--- scripts/resume.sh
+++ scripts/resume.sh
@@ -1,16 +1,42 @@
 #!/usr/bin/env bash
 
-set -ex
+set -eu
 
-declare -r on_resume_command="$1"
+set_options_for_resumed_state() {
+  local -r escaped_delim="${RANDOM}${RANDOM}${RANDOM}"
+
+  local _options="${1#,}"
+  _options="${_options//\\,/${escaped_delim}}"
+  IFS=, read -ra options <<< "${_options}"
+
+  local flags=""
+  local name=""
+  local value=""
+
+  for item in "${options[@]}"; do
+    name="$(echo "${item%%:*}" | xargs)"
+    item="${item#*:}"
+    flags="${item%%:*}"
+    value="${item#*:}"
+    value="${value//${escaped_delim}/,}"
+
+    tmux set-option -q${flags} "${name}" "${value}"
+  done
+}
+
+declare -r on_resume_command="${1}"
+declare -r resumed_options="$(tmux show-option -qv '@suspend_resumed_options')"
+
 declare -r prefix="$(tmux show-option -qv '@suspend_prefix')"
-declare prefix_scope=""
-if [[ -z $prefix ]]; then
-  prefix_scope="u"
+declare prefix_flags=""
+if [[ -z ${prefix} ]]; then
+  prefix_flags="u"
 fi
 
-tmux set-option -q$prefix_scope prefix "$prefix" \; set-option -u key-table
+eval "${on_resume_command}"
 
-eval "$on_resume_command"
+set_options_for_resumed_state "${resumed_options}"
+
+tmux set-option -q${prefix_flags} prefix "${prefix}" \; set-option -u key-table
 
 tmux refresh-client -S
--- scripts/suspend.sh
+++ scripts/suspend.sh
@@ -1,8 +1,46 @@
 #!/usr/bin/env bash
 
-set -ex
+set -eu
 
-declare -r on_suspend_command="$1"
+set_options_for_suspended_state() {
+  local -r escaped_delim="${RANDOM}${RANDOM}${RANDOM}"
+
+  local _options="${1#,}"
+  _options="${_options//\\,/${escaped_delim}}"
+  IFS=, read -ra options <<< "${_options}"
+
+  local flags=""
+  local name=""
+  local value=""
+
+  local resumed_options=""
+  for item in "${options[@]}"; do
+    if [[ -z "$(echo "${item}" | xargs)" ]]; then
+      continue
+    fi
+
+    name="$(echo "${item%%:*}" | xargs)"
+    item="${item#*:}"
+    flags="${item%%:*}"
+    value="${item#*:}"
+    value="${value//${escaped_delim}/,}"
+
+    has_value="$(tmux show-options -qv${flags} "${name}" | wc -l | xargs)"
+    preserved_flags="${flags}"
+    if [[ "${has_value}" = "0" ]]; then
+      preserved_flags="${preserved_flags}u"
+    fi
+    preserved_value="$(tmux show-options -qv${flags} "${name}")"
+    resumed_options="${resumed_options},${name}:${preserved_flags}:${preserved_value//,/\\,}"
+
+    tmux set-option -q${flags} "${name}" "${value}"
+  done
+
+  tmux set-option -q '@suspend_resumed_options' "${resumed_options}"
+}
+
+declare -r on_suspend_command="${1}"
+declare -r suspended_options="${2}"
 
 tmux set-option -q '@suspend_prefix' "$(tmux show-option -qv prefix)"
 
@@ -10,6 +48,8 @@ tmux set-option -q prefix none \; set-option key-table suspended \; \
   if-shell -F '#{pane_in_mode}' 'send-keys -X cancel' \; \
   if-shell -F '#{pane_synchronized}' 'set synchronize-panes off'
 
-eval "$on_suspend_command"
+set_options_for_suspended_state "${suspended_options}"
+
+eval "${on_suspend_command}"
 
 tmux refresh-client -S
